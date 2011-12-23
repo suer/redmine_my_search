5e4b50718e0870cc0de644c8e19086229c5af6f5
suer <suetsugu.r@gmail.com>
2011-12-24 00:31:44 +0900

    install page

--- app/controllers/my_search_controller.rb
+++ app/controllers/my_search_controller.rb
@@ -2,22 +2,11 @@ class MySearchController < ApplicationController
   unloadable
 
   include QueriesHelper
+  include MySearchHelper
 
-  def index
+  def data
     retrieve_query
     @issues = @query.issues
     render :text => @issues.inject('') {|list, issue| list << issue_to_s(issue) }, :content_type => 'text/plain'
   end
-
-  private
-  def issue_to_s(issue)
-    text = ''
-    text << issue.subject
-    text << "\n"
-    text << (issue.description.blank? ? '' : issue.description.gsub(/\r\n|\r|\n/, ''))
-    text << "\n"
-    text << url_for(:controller => 'issues', :action => 'show', :id => issue)
-    text << "\n"
-  end
-
 end
--- app/helpers/my_search_helper.rb
+++ app/helpers/my_search_helper.rb
@@ -1,2 +1,11 @@
 module MySearchHelper
+  def issue_to_s(issue)
+    text = ''
+    text << issue.subject
+    text << "\n"
+    text << (issue.description.blank? ? '' : issue.description.gsub(/\r\n|\r|\n/, ''))
+    text << "\n"
+    text << url_for(:controller => 'issues', :action => 'show', :id => issue)
+    text << "\n"
+  end
 end
