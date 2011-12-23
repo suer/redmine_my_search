331bd91cf5868e7cce4f862e8c63bf2253d6f010
suer <suetsugu.r@gmail.com>
2011-12-24 00:13:04 +0900

    readme

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ require 'redmine'
 Redmine::Plugin.register :redmine_my_search do
   name 'Redmine My Search plugin'
   author 'suer, mallowlabs'
-  description 'This is a plugin for Redmine'
+  description 'This plugin generate text data for searching'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/suer/redmine_my_search'
   author_url 'http://d.hatena.ne.jp/suer/'
 end
