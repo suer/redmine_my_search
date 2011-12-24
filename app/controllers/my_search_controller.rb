fb0abfb10994d3f99f2e6db5d5cf5b06d8371edb
suer <sue@nagoya-u.jp>
2011-12-24 15:49:54 +0900

    accept api key

--- app/controllers/my_search_controller.rb
+++ app/controllers/my_search_controller.rb
@@ -1,12 +1,18 @@
 class MySearchController < ApplicationController
   unloadable
+  accept_key_auth :data
 
   include QueriesHelper
   include MySearchHelper
 
   def data
     retrieve_query
-    @issues = Issue.visible.find(:all, :order => "issues.id desc")
-    render :text => @issues.inject('') {|list, issue| list << issue_to_s(issue) }, :content_type => 'text/plain'
+    issues = Issue.visible.find(:all, :order => "issues.id desc")
+    render :text => issues.inject('') {|list, issue| list << issue_to_s(issue) }, :content_type => 'text/plain'
   end
+
+  def api_request?
+    %w(text).include? params[:format]
+  end
+
 end
