e52a1e6be9a561a5bf63fd5310d8f78fb066c05e
suer <sue@nagoya-u.jp>
2011-12-24 12:32:42 +0900

    sort desc

--- app/controllers/my_search_controller.rb
+++ app/controllers/my_search_controller.rb
@@ -6,7 +6,7 @@ class MySearchController < ApplicationController
 
   def data
     retrieve_query
-    @issues = Issue.visible.find(:all)
+    @issues = Issue.visible.find(:all, :order => "issues.id desc")
     render :text => @issues.inject('') {|list, issue| list << issue_to_s(issue) }, :content_type => 'text/plain'
   end
 end
