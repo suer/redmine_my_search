13deaad2b6c6100d6e33fca286d1301c79372496
suer <suetsugu.r@gmail.com>
2012-04-22 02:22:07 +0900

    Fixed: no routes found

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect '/my_search/data.:format', :controller => 'my_search', :action => 'data'
+  map.connect '/my_search/:action.:format', :controller => 'my_search'
+ # map.connect '/my_search/data.:format', :controller => 'my_search', :action => 'data'
 end
