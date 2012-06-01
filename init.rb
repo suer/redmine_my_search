d74dceefb6dc0755eaed1a2d8b911c34f3df3708
suer <suetsugu.r@gmail.com>
2012-06-01 10:38:04 +0900

    0.0.3

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_my_search do
   name 'Redmine My Search plugin'
   author 'suer, mallowlabs'
   description 'This plugin generate text data for searching'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/suer/redmine_my_search'
   author_url 'http://d.hatena.ne.jp/suer/'
 end
