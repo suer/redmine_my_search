18de60dc0ed498874351033d7d8c0505da518ca7
suer <suetsugu.r@gmail.com>
2012-07-02 00:30:20 +0900

    toggle box

--- assets/javascripts/my_search.js
+++ assets/javascripts/my_search.js
@@ -1,3 +1,18 @@
 // my search plugin
 
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 191) {
+        toggleBox();
+    }
+});
 
+function toggleBox() {
+    var display = $('my-search-box').style.display;
+    if (display == 'none') {
+        $('my-search-box').style.display = 'block';
+        $('my-search-input').focus();
+    } else {
+        $('my-search-box').style.display = 'none';
+    }
+
+}
--- lib/my_search_view_listener.rb
+++ lib/my_search_view_listener.rb
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
 class MySearchViewListener < Redmine::Hook::ViewListener
   def view_layouts_base_content(context)
-    html = '<div id="my-search-box"></div>'
+    html = '<div id="my-search-box">'
+    html << '<div><input id="my-search-input" type="text"></input></div>'
+    html << '<div id="my-search-status-line">'
+    html << '<span id="project-name"></span>'
+    html << '<span></span>'
+    html << '</div>'
+    html << '</div>'
     js_url = Redmine::Utils.relative_url_root + '/plugin_assets/redmine_my_search/javascripts/my_search.js'
     html << javascript_include_tag(js_url)
     css_url = Redmine::Utils.relative_url_root + '/plugin_assets/redmine_my_search/stylesheets/my_search.css'
