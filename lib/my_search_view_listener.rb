ba6ac6dc5bd9d81834dc88203828962c3b48f116
suer <suetsugu.r@gmail.com>
2012-07-01 23:26:19 +0900

    add js and css

--- lib/my_search_view_listener.rb
+++ lib/my_search_view_listener.rb
@@ -1,5 +1,9 @@
 class MySearchViewListener < Redmine::Hook::ViewListener
   def view_layouts_base_content(context)
-    '<div id="my-search-box"></div>'
+    html = '<div id="my-search-box"></div>'
+    js_url = Redmine::Utils.relative_url_root + '/plugin_assets/redmine_my_search/javascripts/my_search.js'
+    html << javascript_include_tag(js_url)
+    css_url = Redmine::Utils.relative_url_root + '/plugin_assets/redmine_my_search/stylesheets/my_search.css'
+    html << stylesheet_link_tag(css_url)
   end
 end
