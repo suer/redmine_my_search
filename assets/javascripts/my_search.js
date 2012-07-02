d3c4dcce92d33aa5059f2f46403f9fcde96dd10d
suer <suetsugu.r@gmail.com>
2012-07-02 23:23:07 +0900

    fixed: indent

--- assets/javascripts/my_search.js
+++ assets/javascripts/my_search.js
@@ -1,83 +1,83 @@
 // my search plugin
 
 (function(document, window, undefined) {
-var data = [];
-var selectedData = null;
-document.addEventListener('keydown', function(e) {
-    if (e.ctrlKey && e.keyCode == 71) {
-        toggleBox();
-        if (data.length == 0) {
-            loadData();
+    var data = [];
+    var selectedData = null;
+    document.addEventListener('keydown', function(e) {
+        if (e.ctrlKey && e.keyCode == 71) {
+            toggleBox();
+            if (data.length == 0) {
+                loadData();
+            }
+        }
+    });
+
+    $('my-search-input').addEventListener('keydown', function(e) {
+        if (e.ctrlKey && e.keyCode == 71) {
+            return;
+        }
+        if (e.keyCode == 13 && selectedData) {
+            location.href = selectedData['url'];
+        }
+        var value = $('my-search-input').value;
+        if (value == null || value.length == 0) {
+            return;
+        }
+        var len = data.length;
+        var regExp = new RegExp(value);
+        for (var i = 0; i < len; i++) {
+            target = data[i]['title'] + data[i]['description'];
+            if (target.match(regExp)) {
+                $('my-search-title').innerHTML = data[i]['title'];
+                $('my-search-description').innerHTML = data[i]['description'];
+                $('my-search-url').innerHTML = data[i]['url'];
+                selectedData = data[i];
+            }
+        }
+    });
+
+    function toggleBox() {
+        var display = $('my-search-box').style.display;
+        if (display == 'none') {
+            $('my-search-box').style.display = 'block';
+            $('my-search-input').focus();
+        } else {
+            $('my-search-box').style.display = 'none';
+        }
+
+    }
+
+    function loadData() {
+        new Ajax.Request(
+            getBaseURL() + '/my_search/data_all',
+            {
+                method: 'get',
+                parameters: '',
+                onComplete: function (request) {
+                    parseData(request.responseText);
+                }
+            });
+    }
+
+    function parseData(text) {
+        var textArray = text.split(/\r\n|\r|\n/);
+        var len = textArray.length;
+        var obj = {}
+        for (var i = 0; i < len; i++) {
+            if (i % 3 == 0) {
+                obj['title'] = textArray[i];
+            } else if (i % 3 == 1) {
+                obj['description'] = textArray[i];
+            } else if (i % 3 == 2) {
+                obj['url'] = textArray[i];
+                data.push(obj);
+                obj = {};
+            }
         }
     }
-});
 
-$('my-search-input').addEventListener('keydown', function(e) {
-    if (e.ctrlKey && e.keyCode == 71) {
-        return;
+    function getBaseURL() {
+        return document.getElementsByTagName('script')[0].src.split('/javascripts')[0];
     }
-    if (e.keyCode == 13 && selectedData) {
-        location.href = selectedData['url'];
-    }
-    var value = $('my-search-input').value;
-    if (value == null || value.length == 0) {
-        return;
-    }
-    var len = data.length;
-    var regExp = new RegExp(value);
-    for (var i = 0; i < len; i++) {
-        target = data[i]['title'] + data[i]['description'];
-        if (target.match(regExp)) {
-            $('my-search-title').innerHTML = data[i]['title'];
-            $('my-search-description').innerHTML = data[i]['description'];
-            $('my-search-url').innerHTML = data[i]['url'];
-            selectedData = data[i];
-        }
-    }
-});
-
-function toggleBox() {
-    var display = $('my-search-box').style.display;
-    if (display == 'none') {
-        $('my-search-box').style.display = 'block';
-        $('my-search-input').focus();
-    } else {
-        $('my-search-box').style.display = 'none';
-    }
-
-}
-
-function loadData() {
-    new Ajax.Request(
-        getBaseURL() + '/my_search/data_all',
-        {
-          method: 'get',
-          parameters: '',
-          onComplete: function (request) {
-            parseData(request.responseText);
-          }
-        });
-}
-
-function parseData(text) {
-    var textArray = text.split(/\r\n|\r|\n/);
-    var len = textArray.length;
-    var obj = {}
-    for (var i = 0; i < len; i++) {
-        if (i % 3 == 0) {
-            obj['title'] = textArray[i];
-        } else if (i % 3 == 1) {
-            obj['description'] = textArray[i];
-        } else if (i % 3 == 2) {
-            obj['url'] = textArray[i];
-            data.push(obj);
-            obj = {};
-        }
-    }
-}
-
-function getBaseURL() {
-    return document.getElementsByTagName('script')[0].src.split('/javascripts')[0];
-}
 
 })(document, window);
